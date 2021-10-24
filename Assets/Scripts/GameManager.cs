f084fff5427c2ba6334a3cdea9e8ccaf6433a583
Techj70 <65133090+Techj70@users.noreply.github.com>
2021-10-24 05:08:32 -0400

    pushed changes to GameManager.cs that were missing from last commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,30 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using Scene = UnityEngine.SceneManagement.Scene;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("Scene/Level Variables")]
-    public static float LevelTimeLimit = 120f; // set default time time for the level to 2 minutes
+    [Header("Level Variables")]
+    public float levelTimeLimit = 120f; // set default time time for the level to 2 minutes
+    
+    public int levelScoreBronze;
+    public int levelScoreSliver;
+    public int levelScoreGold;
 
+    
+    
     [Header("References")]
     public TMP_Text timeValue;
 
+    public SceneAsset currentLevel;
+    public SceneAsset nextLevel;
+    public SceneAsset menu;
+
     private float _minutes;
     private float _seconds;
+    private int x = 0;
     
     
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        _minutes = Mathf.RoundToInt(LevelTimeLimit / 60);     // sets _minutes variable to rounded integer after dividing remaining time by 6o
-        _seconds = Mathf.RoundToInt(LevelTimeLimit % 60);     // sets _seconds variable to rounded integer after getting the remainder of the division of remaining time by 60
+        _minutes = Mathf.RoundToInt(levelTimeLimit / 60);     // sets _minutes variable to rounded integer after dividing remaining time by 6o
+        _seconds = Mathf.RoundToInt(levelTimeLimit % 60);     // sets _seconds variable to rounded integer after getting the remainder of the division of remaining time by 60
     }
+    
 
     // Update is called once per frame
     void Update()
+    {
+        TimeKeeper();
+        if (levelTimeLimit <= 0 && ScoreKeeper.LevelScore <= levelScoreBronze)
+            Lose();
+        if (levelTimeLimit <= 0 && ScoreKeeper.LevelScore >= levelScoreBronze)
+        {
+            if (ScoreKeeper.LevelScore >= levelScoreBronze && ScoreKeeper.LevelScore < levelScoreSliver)
+            {
+                Win(1);  
+            }
+            else if (ScoreKeeper.LevelScore >= levelScoreSliver && ScoreKeeper.LevelScore < levelScoreGold)
+            {
+                Win(2);
+            }
+            else if (ScoreKeeper.LevelScore >= levelScoreGold)
+            {
+                Win(3);
+            }
+        }
+    }
+
+    public void TimeKeeper () // keeps time for "Time Remaining" in the UI and for the game
     {
         if (_seconds <= 0)
         {
@@ -32,7 +70,25 @@ public class GameManager : MonoBehaviour
             _seconds = 59f;
         }
         
-        _seconds -= Time.deltaTime;                       // decreased remaining time by 1 second
-        timeValue.text = _minutes.ToString("00") + ":" + Mathf.RoundToInt(_seconds).ToString("00");             // displays the time in minutes and seconds to the timeValue TextMeshPro referenced in the Inspector
+        _seconds -= Time.deltaTime;   // decreased remaining time by 1 second
+        timeValue.text = _minutes.ToString("00") + ":" + Mathf.RoundToInt(_seconds).ToString("00");  // displays the time in minutes and seconds to the timeValue TextMeshPro referenced in the Inspector
+    }
+
+    public void Win(int scoreTier)
+    {
+        switch (scoreTier)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+    }
+
+    public void Lose()
+    {
+        
     }
 }
