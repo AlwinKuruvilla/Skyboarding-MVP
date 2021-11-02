96f4ea372f7c8e29c2b8fb7e9129bc3327b9f777
Emma <emma.ym.zhao@gmail.com>
2021-11-02 01:07:10 -0400

    basic targeting HUD done

---
+++ Assets/Scripts/Minimap/TransformExtensions.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+ 
+public static class TransformExtensions
+{
+    // from here https://answers.unity.com/questions/1238142/version-of-transformtransformpoint-which-is-unaffe.html
+    
+    public static Vector3 TransformPointUnscaled(this Transform transform, Vector3 position)
+    {
+        var localToWorldMatrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+        return localToWorldMatrix.MultiplyPoint3x4(position);
+    }
+ 
+    public static Vector3 InverseTransformPointUnscaled(this Transform transform, Vector3 position)
+    {
+        var worldToLocalMatrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one).inverse;
+        return worldToLocalMatrix.MultiplyPoint3x4(position);
+    }
+}
