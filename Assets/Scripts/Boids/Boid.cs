8356cd6a63f86dbc483ee3c9d9be046ac5962454
Alwin Kuruvilla <alwin@akxlgames.com>
2021-11-04 23:09:51 -0400

    Boids colored based on evasion type.

--- Assets/Scripts/Boids/Boid.cs
+++ Assets/Scripts/Boids/Boid.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Boid : MonoBehaviour {
 	BoidSettings settings;
@@ -10,7 +11,9 @@ public class Boid : MonoBehaviour {
 	[HideInInspector] public Vector3 position;
 	[HideInInspector] public Vector3 forward;
 	Vector3 velocity;
-	public bool isEvading;
+	[HideInInspector] public bool isEvading;
+	private bool isCaptured;
+	private EvasionType _evasionType;
 
 	// To update:
 	Vector3 acceleration;
@@ -23,6 +26,13 @@ public class Boid : MonoBehaviour {
 	Material material;
 	Transform cachedTransform;
 	Transform target;
+	
+	public enum EvasionType {
+		EvadeUp = 0,
+		EvadeDown = 1,
+		EvadeLeft = 2,
+		EvadeRight = 3
+	}
 
 	void Awake() {
 		material = transform.GetComponentInChildren<MeshRenderer>().material;
@@ -38,58 +48,99 @@ public class Boid : MonoBehaviour {
 
 		float startSpeed = (settings.minSpeed + settings.maxSpeed) / 2;
 		velocity = transform.forward * startSpeed;
+
+		_evasionType = (EvasionType)Random.Range(0, 4);
+		SetColour();
 	}
 
-	public void SetColour(Color col) {
-		if (material != null) {
-			material.color = col;
+	public void SetColour() {
+		switch (_evasionType) {
+			case EvasionType.EvadeUp:
+				material.color = Color.red;
+				break;
+			case EvasionType.EvadeDown:
+				material.color = Color.blue;
+				break;
+			case EvasionType.EvadeLeft:
+				material.color = Color.green;
+				break;
+			case EvasionType.EvadeRight:
+				material.color = Color.yellow;
+				break;
+			default:
+				material.color = Color.black;
+				break;
 		}
 	}
 
 	public void UpdateBoid() {
 		Vector3 acceleration = Vector3.zero;
+		if (!isCaptured) {
+			if (target != null) {
+				Vector3 offsetToTarget = (target.position - position);
+				acceleration = SteerTowards(offsetToTarget) * settings.targetWeight;
+			}
 
-		if (target != null) {
-			Vector3 offsetToTarget = (target.position - position);
-			acceleration = SteerTowards(offsetToTarget) * settings.targetWeight;
+			if (numPerceivedFlockmates != 0) {
+				centreOfFlockmates /= numPerceivedFlockmates;
+
+				Vector3 offsetToFlockmatesCentre = (centreOfFlockmates - position);
+
+				var alignmentForce = SteerTowards(avgFlockHeading) * settings.alignWeight;
+				var cohesionForce = SteerTowards(offsetToFlockmatesCentre) * settings.cohesionWeight;
+				var seperationForce = SteerTowards(avgAvoidanceHeading) * settings.seperateWeight;
+
+				acceleration += alignmentForce;
+				acceleration += cohesionForce;
+				acceleration += seperationForce;
+			}
+
+			if (IsHeadingForCollision()) {
+				Vector3 collisionAvoidDir = ObstacleRays();
+				Vector3 collisionAvoidForce = SteerTowards(collisionAvoidDir) * settings.avoidCollisionWeight;
+				acceleration += collisionAvoidForce;
+			}
+
+			if (isEvading) {
+				Vector3 evadeForce = SteerTowards(EvasionDir()) * settings.evadeWeight;
+				acceleration = evadeForce;
+				isEvading = false;
+			}
+
+			velocity += acceleration * Time.deltaTime;
+			float speed = velocity.magnitude;
+			Vector3 dir = velocity / speed;
+			speed = Mathf.Clamp(speed, settings.minSpeed, settings.maxSpeed);
+			velocity = dir * speed;
+
+			cachedTransform.position += velocity * Time.deltaTime;
+			cachedTransform.forward = dir;
+			position = cachedTransform.position;
+			forward = dir;
 		}
+	}
 
-		if (numPerceivedFlockmates != 0) {
-			centreOfFlockmates /= numPerceivedFlockmates;
-
-			Vector3 offsetToFlockmatesCentre = (centreOfFlockmates - position);
-
-			var alignmentForce = SteerTowards(avgFlockHeading) * settings.alignWeight;
-			var cohesionForce = SteerTowards(offsetToFlockmatesCentre) * settings.cohesionWeight;
-			var seperationForce = SteerTowards(avgAvoidanceHeading) * settings.seperateWeight;
-
-			acceleration += alignmentForce;
-			acceleration += cohesionForce;
-			acceleration += seperationForce;
+	private Vector3 EvasionDir() {
+		switch (_evasionType) {
+			case EvasionType.EvadeUp:
+				return Vector3.up;
+			case EvasionType.EvadeDown:
+				return Vector3.down;
+			case EvasionType.EvadeLeft:
+				return Vector3.left;
+			case EvasionType.EvadeRight:
+				return Vector3.right;
+			default:
+				return Vector3.back;
 		}
+	}
 
-		if (IsHeadingForCollision()) {
-			Vector3 collisionAvoidDir = ObstacleRays();
-			Vector3 collisionAvoidForce = SteerTowards(collisionAvoidDir) * settings.avoidCollisionWeight;
-			acceleration += collisionAvoidForce;
-		}
+	public void IsCaptured() {
+		isCaptured = true;
+	}
 
-		if (isEvading) {
-			Vector3 evadeForce = SteerTowards(Vector3.up) * settings.evadeWeight;
-			acceleration = evadeForce;
-			isEvading = false;
-		}
-		
-		velocity += acceleration * Time.deltaTime;
-		float speed = velocity.magnitude;
-		Vector3 dir = velocity / speed;
-		speed = Mathf.Clamp(speed, settings.minSpeed, settings.maxSpeed);
-		velocity = dir * speed;
-
-		cachedTransform.position += velocity * Time.deltaTime;
-		cachedTransform.forward = dir;
-		position = cachedTransform.position;
-		forward = dir;
+	public void IsReleased() {
+		isCaptured = false;
 	}
 
 	bool IsHeadingForCollision() {
