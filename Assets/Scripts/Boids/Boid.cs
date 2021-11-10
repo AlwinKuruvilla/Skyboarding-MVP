1e62e0171b547ffcc36a2743f1e67d8beee89076
Alwin Kuruvilla <alwin@akxlgames.com>
2021-11-10 10:14:32 -0500

    Working on boid's shader

--- Assets/Scripts/Boids/Boid.cs
+++ Assets/Scripts/Boids/Boid.cs
@@ -5,27 +5,26 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class Boid : MonoBehaviour {
-	BoidSettings settings;
+	BoidSettings _settings;
 
 	// State
 	[HideInInspector] public Vector3 position;
 	[HideInInspector] public Vector3 forward;
-	Vector3 velocity;
+	Vector3 _velocity;
 	[HideInInspector] public bool isEvading;
-	private bool isCaptured;
+	private bool _isCaptured;
 	private EvasionType _evasionType;
 
 	// To update:
-	Vector3 acceleration;
 	[HideInInspector] public Vector3 avgFlockHeading;
 	[HideInInspector] public Vector3 avgAvoidanceHeading;
 	[HideInInspector] public Vector3 centreOfFlockmates;
 	[HideInInspector] public int numPerceivedFlockmates;
 
 	// Cached
-	Material material;
-	Transform cachedTransform;
-	Transform target;
+	Material _material;
+	Transform _cachedTransform;
+	Transform _target;
 	
 	public enum EvasionType {
 		EvadeUp = 0,
@@ -35,50 +34,50 @@ public class Boid : MonoBehaviour {
 	}
 
 	void Awake() {
-		material = transform.GetComponentInChildren<MeshRenderer>().material;
-		cachedTransform = transform;
+		//_material = transform.GetComponentInChildren<MeshRenderer>().material;
+		_cachedTransform = transform;
 	}
 
 	public void Initialize(BoidSettings settings, Transform target) {
-		this.target = target;
-		this.settings = settings;
+		this._target = target;
+		this._settings = settings;
 
-		position = cachedTransform.position;
-		forward = cachedTransform.forward;
+		position = _cachedTransform.position;
+		forward = _cachedTransform.forward;
 
 		float startSpeed = (settings.minSpeed + settings.maxSpeed) / 2;
-		velocity = transform.forward * startSpeed;
+		_velocity = transform.forward * startSpeed;
 
 		_evasionType = (EvasionType)Random.Range(0, 4);
-		SetColour();
+		//SetColour();
 	}
 
-	public void SetColour() {
-		switch (_evasionType) {
-			case EvasionType.EvadeUp:
-				material.color = Color.red;
-				break;
-			case EvasionType.EvadeDown:
-				material.color = Color.blue;
-				break;
-			case EvasionType.EvadeLeft:
-				material.color = Color.green;
-				break;
-			case EvasionType.EvadeRight:
-				material.color = Color.yellow;
-				break;
-			default:
-				material.color = Color.black;
-				break;
-		}
-	}
+	// public void SetColour() {
+	// 	switch (_evasionType) {
+	// 		case EvasionType.EvadeUp:
+	// 			_material.color = Color.red;
+	// 			break;
+	// 		case EvasionType.EvadeDown:
+	// 			_material.color = Color.blue;
+	// 			break;
+	// 		case EvasionType.EvadeLeft:
+	// 			_material.color = Color.green;
+	// 			break;
+	// 		case EvasionType.EvadeRight:
+	// 			_material.color = Color.yellow;
+	// 			break;
+	// 		default:
+	// 			_material.color = Color.black;
+	// 			break;
+	// 	}
+	// }
 
 	public void UpdateBoid() {
 		Vector3 acceleration = Vector3.zero;
-		if (!isCaptured) {
-			if (target != null) {
-				Vector3 offsetToTarget = (target.position - position);
-				acceleration = SteerTowards(offsetToTarget) * settings.targetWeight;
+		if (!_isCaptured) {
+			if (_target != null) {
+				Vector3 offsetToTarget = (_target.position - position);
+				acceleration = SteerTowards(offsetToTarget) * _settings.targetWeight;
 			}
 
 			if (numPerceivedFlockmates != 0) {
@@ -86,9 +85,9 @@ public class Boid : MonoBehaviour {
 
 				Vector3 offsetToFlockmatesCentre = (centreOfFlockmates - position);
 
-				var alignmentForce = SteerTowards(avgFlockHeading) * settings.alignWeight;
-				var cohesionForce = SteerTowards(offsetToFlockmatesCentre) * settings.cohesionWeight;
-				var seperationForce = SteerTowards(avgAvoidanceHeading) * settings.seperateWeight;
+				var alignmentForce = SteerTowards(avgFlockHeading) * _settings.alignWeight;
+				var cohesionForce = SteerTowards(offsetToFlockmatesCentre) * _settings.cohesionWeight;
+				var seperationForce = SteerTowards(avgAvoidanceHeading) * _settings.seperateWeight;
 
 				acceleration += alignmentForce;
 				acceleration += cohesionForce;
@@ -97,25 +96,25 @@ public class Boid : MonoBehaviour {
 
 			if (IsHeadingForCollision()) {
 				Vector3 collisionAvoidDir = ObstacleRays();
-				Vector3 collisionAvoidForce = SteerTowards(collisionAvoidDir) * settings.avoidCollisionWeight;
+				Vector3 collisionAvoidForce = SteerTowards(collisionAvoidDir) * _settings.avoidCollisionWeight;
 				acceleration += collisionAvoidForce;
 			}
 
 			if (isEvading) {
-				Vector3 evadeForce = SteerTowards(EvasionDir()) * settings.evadeWeight;
+				Vector3 evadeForce = SteerTowards(EvasionDir()) * _settings.evadeWeight;
 				acceleration = evadeForce;
 				isEvading = false;
 			}
 
-			velocity += acceleration * Time.deltaTime;
-			float speed = velocity.magnitude;
-			Vector3 dir = velocity / speed;
-			speed = Mathf.Clamp(speed, settings.minSpeed, settings.maxSpeed);
-			velocity = dir * speed;
+			_velocity += acceleration * Time.deltaTime;
+			float speed = _velocity.magnitude;
+			Vector3 dir = _velocity / speed;
+			speed = Mathf.Clamp(speed, _settings.minSpeed, _settings.maxSpeed);
+			_velocity = dir * speed;
 
-			cachedTransform.position += velocity * Time.deltaTime;
-			cachedTransform.forward = dir;
-			position = cachedTransform.position;
+			_cachedTransform.position += _velocity * Time.deltaTime;
+			_cachedTransform.forward = dir;
+			position = _cachedTransform.position;
 			forward = dir;
 		}
 	}
@@ -136,17 +135,17 @@ public class Boid : MonoBehaviour {
 	}
 
 	public void IsCaptured() {
-		isCaptured = true;
+		_isCaptured = true;
 	}
 
 	public void IsReleased() {
-		isCaptured = false;
+		_isCaptured = false;
 	}
 
 	bool IsHeadingForCollision() {
 		RaycastHit hit;
-		if (Physics.SphereCast(position, settings.boundsRadius, forward, out hit, settings.collisionAvoidDst,
-			settings.obstacleMask)) {
+		if (Physics.SphereCast(position, _settings.boundsRadius, forward, out hit, _settings.collisionAvoidDst,
+			_settings.obstacleMask)) {
 			return true;
 		}
 
@@ -168,9 +167,9 @@ public class Boid : MonoBehaviour {
 		Vector3[] rayDirections = BoidHelper.directions;
 
 		for (int i = 0; i < rayDirections.Length; i++) {
-			Vector3 dir = cachedTransform.TransformDirection(rayDirections[i]);
+			Vector3 dir = _cachedTransform.TransformDirection(rayDirections[i]);
 			Ray ray = new Ray(position, dir);
-			if (!Physics.SphereCast(ray, settings.boundsRadius, settings.collisionAvoidDst, settings.obstacleMask)) {
+			if (!Physics.SphereCast(ray, _settings.boundsRadius, _settings.collisionAvoidDst, _settings.obstacleMask)) {
 				return dir;
 			}
 		}
@@ -180,7 +179,7 @@ public class Boid : MonoBehaviour {
 	
 
 	Vector3 SteerTowards(Vector3 vector) {
-		Vector3 v = vector.normalized * settings.maxSpeed - velocity;
-		return Vector3.ClampMagnitude(v, settings.maxSteerForce);
+		Vector3 v = vector.normalized * _settings.maxSpeed - _velocity;
+		return Vector3.ClampMagnitude(v, _settings.maxSteerForce);
 	}
 }
